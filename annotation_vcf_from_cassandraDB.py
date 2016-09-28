aad2d5955c625c8078c388a3d2f6e24dda40fd45
Zhengxuan Wu <zxw0424@gmail.com>
2016-09-28 02:52:04 -0400

    SEV-002 Continue
    TODO: deserialise the json annotation, multiprocessing return

--- annotation_vcf_from_cassandraDB.py
+++ annotation_vcf_from_cassandraDB.py
@@ -1,22 +1,40 @@
 '''
     multi-processing data retrieving of VCF from cassandraDB
 '''
+
+# Some references:
 # http://kmdouglass.github.io/posts/learning-pythons-multiprocessing-module.html
+# http://stackoverflow.com/questions/20887555/dead-simple-example-of-using-multiprocessing-queue-pool-and-locking
+# http://www.datastax.com/dev/blog/datastax-python-driver-multiprocessing-example-for-improved-bulk-data-throughput
+
+'''Avoid using multi-threading, because in python it is serialized threading,
+    pooled multi-processing will reduce the overhead, as well as maximize the
+    parallel efficiency in the program'''
 
 
-import gzip, os, sys, csv, re
+import gzip, os, sys, csv, re, multiprocessing
+from cassandra import ConsistencyLevel
+from cassandra.cluster import Cluster
 
 class annotator_vcf(object):
     '''annotator for plain vcf file, fetching data from cassandraDB'''
 
-    def __init__(self, input_filename, output_filename, contact_point_DB, keyspace_DB, table_DB):
+    def __init__(self, input_filename, output_filename, contact_point_DB, keyspace_DB, table_DB, concurrent_number):
         '''Initialize function'''
         self.input_filename = input_filename
         self.output_filename = output_filename
         self.contact_point_DB = contact_point_DB
         self.keyspace_DB = keyspace_DB
         self.table_DB = table_DB
+        self.concurrent_number = concurrent_number
 
+        # establish the connection to the database
+        # entering the pre-defined keyspace
+        cluster = Cluster(contact_points=self.contact_points)
+        session = cluster.connect()
+        session.set_keyspace(self.keyspace)
+        
+        # database variables
         self.primary_field = ["CHROM", "POS", "REF", "ALT"]
         self.primary_key = []
         self.fields = None
@@ -46,9 +64,37 @@ class annotator_vcf(object):
             # Append to the primary key list
             self.primary_key.append(sample_primary_key)
 
+    def run(self, sample):
+        '''single process run function'''
+        '''will return a single row in the whole table, iterative'''
+        # isolation creation of the connection and executions
+        profile_stmt = session.prepare("SELECT annotations From" + self.table_DB +\
+                                       " Where chrom = ? AND pos = ? AND ref = ? \
+                                       AND alt = ?")
+        bound_stmt = profile_stmt.bind(sample)
+        single_annotation_json = session.execute(bound_stmt)
+
+        # TODO://
+        
+        # deserialize
+        
+        
+        # combine and return
     
-    def annotate(self):
+    
+    def json_to_vep(annotation_json):
+        '''deserialize the python json string format to a VEP format'''
+
+
+    def multiprocessing_annotate(self):
         '''Main method to annotate the vcf file, and output a csv file'''
+        # calling to generate the primary key
+        self.generate_primary_key()
+        
+        # creating processing pools
+        p = multiprocessing.Pool(self.concurrent_number)
+        annotated_list = p.map(self.run(), self.primary_key)
+        
         # file open for writing
         w = open(self.output_filename, 'w')
         wr = csv.writer(w, dialect='excel')
@@ -59,6 +105,7 @@ class annotator_vcf(object):
         wr.writerow(csv_header)
         
         # file open for reading
-        f = open(self.input_filename, 'rb')
+        for line in annotated_list:
+            wr.writerow(line)
 
 
