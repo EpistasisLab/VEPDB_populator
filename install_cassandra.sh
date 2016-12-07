ee24280306dc1454de44ee02d07f6d509a2675ad
Brian S. Cole, PhD <bryketos@gmail.com>
2016-12-07 11:58:49 -0500

    Updated cassandra repo versions to 3.0.9.1

--- install_cassandra.sh
+++ install_cassandra.sh
@@ -16,8 +16,8 @@ printf '%s\n%s\n%s\n%s\n%s\n' '[datastax]' 'name = DataStax Repo for Apache Cass
 #for a list of available versions, see http://rpm.datastax.com/community/noarch/
 #Here I specify Cassandra v3.0.5, because existing VepDB images are using this version, and we need to use this exact version to avoid version difference when adding new nodes to an existing cluster. It’s certainly OK to update the version if we are building a brand new VepDB
 
-yum install dsc30-3.0.5-1 -y
-yum install cassandra30-tools-3.0.5-1 -y
+yum install dsc30-3.0.9-1 -y
+yum install cassandra30-tools-3.0.9-1 -y
 #pip install cqlsh
 #cqlsh will come with the above.
 
